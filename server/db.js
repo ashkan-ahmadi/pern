7225c5a705f9bf1b99406173e3ed5e1ac92fd893
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2023-09-13 23:50:53 +0200

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,5 +2,11 @@ const env = require('./env.json')
 const pg = require('pg')
 
 const pool = new Pool({
-  user: '',
+  user: env?.db?.user,
+  password: env?.db?.password,
+  host: env?.db?.host,
+  port: env?.db?.port,
+  database: env?.db?.database,
 })
+
+module.exports = pool
