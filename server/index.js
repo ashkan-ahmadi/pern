574888121e667ba8dea615778354dbb1fc116895
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2023-09-14 00:06:26 +0200

    Create POST request of todos

--- server/index.js
+++ server/index.js
@@ -13,6 +13,27 @@ app.use(express.json())
 /* APP ROUTES */
 /* CREATE a todo */
 
+app.post('/todos', async (req, res) => {
+  try {
+    const { description } = req.body
+
+    if (!description) {
+      res.status(400).json({
+        success: false,
+        message: 'Invalid data',
+      })
+    }
+    const newTodo = await pool.query('INSERT INTO todo (description) VALUES ($1)', [description])
+
+    res.status(201).json({
+      success: true,
+      ...newTodo,
+    })
+  } catch (error) {
+    console.error(error.message)
+  }
+})
+
 /* GET all todos */
 
 /* GET a todo */
