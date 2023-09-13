445300ba7e6d1c1b03ae742d5a7d21e2c234e1e4
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2023-09-13 23:54:21 +0200

    Add app routes as comments

--- server/index.js
+++ server/index.js
@@ -4,10 +4,22 @@ const env = require('./env.json')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const pool = require('./db')
 
 /* EXPRESS MIDDLEWARES */
 app.use(cors())
 app.use(express.json())
+
+/* APP ROUTES */
+/* CREATE a todo */
+
+/* GET all todos */
+
+/* GET a todo */
+
+/* UPDATE a todo */
+
+/* DELETE a todo */
 
 const SERVER_PORT = env?.port || 5000
 app.listen(SERVER_PORT, () => {
