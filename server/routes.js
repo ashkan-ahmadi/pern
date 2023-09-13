ff1fc7afe1e9dcbd75b0c19247c2e68da4f911c6
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2023-09-14 01:04:59 +0200

    Add PUT route

--- server/routes.js
+++ server/routes.js
@@ -55,13 +55,35 @@ router.get('/todos/:id', async (req, res) => {
     console.error(error.message)
   }
 })
+
 /* UPDATE a todo */
+router.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params // this must match whatever you put as dynamic route
+    const { description } = req.body
+    if (!description) {
+      res.status(400).json({
+        success: false,
+        message: 'Invalid data',
+      })
+    }
+    const todo = await pool.query('UPDATE todo SET description=$1 WHERE todo_id = $2 RETURNING *', [description, id])
+
+    res.status(200).json({
+      success: true,
+      count: todo?.rowCount,
+      data: todo?.rows,
+    })
+  } catch (error) {
+    console.error(error.message)
+  }
+})
 
 /* DELETE a todo */
 router.delete('/todos/:id', async (req, res) => {
   try {
     const { id } = req.params // this must match whatever you put as dynamic route
-    const todo = await pool.query('DELETE FROM todo WHERE todo_id = $1', [id])
+    const todo = await pool.query('DELETE FROM todo WHERE todo_id = $1 RETURNING *', [id])
 
     res.status(200).json({
       success: true,
@@ -71,4 +93,5 @@ router.delete('/todos/:id', async (req, res) => {
     console.error(error.message)
   }
 })
+
 module.exports = router
