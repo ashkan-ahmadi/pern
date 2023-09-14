dacb96333078a4484b7ae9a1006972d1b57bfa78
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2023-09-15 00:39:01 +0200

    Create knexfile

---
+++ server/db/knexfile.js
@@ -0,0 +1,51 @@
+const env = require('../env.json')
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: env?.db?.database,
+      user: env?.db?.user,
+      password: env?.db?.password,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: '',
+      user: '',
+      password: '',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: '',
+      user: '',
+      password: '',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+}
