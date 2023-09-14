d4d12380cb855d597d2a46c568393003c79443a0
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2023-09-15 00:28:18 +0200

    Update app

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+import ListTodos from './components/ListTodos'
+
+import InputTodo from './components/InputTodo'
 import env from './env'
 
 const App = () => {
-  const onSubmit = async e => {
-    e.preventDefault()
+  const [todos, setTodos] = useState([])
 
+  const getTodos = async () => {
     try {
       const host = `${env?.api?.host}/todos`
-      const request = await fetch(host, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ description }),
-      })
+      const request = await fetch(host)
       const response = await request.json()
-      console.log(response)
+
+      if (!response?.success) throw Error('There was a problem fetching the todos.')
+
+      setTodos(response?.data)
     } catch (error) {
       console.error('error onSubmit', error)
     }
   }
 
-  const [description, setDescription] = useState('')
+  useEffect(() => {
+    getTodos()
+  }, [])
 
   return (
     <>
       <main className="container">
         <h1 className="text-center mt-5">TODO</h1>
-        <form onSubmit={onSubmit}>
-          <input type="text" className="form-control" id="description" placeholder="name@example.com" value={description} onChange={e => setDescription(e.target.value)} />
-          <button className="btn btn-primary">+ Add</button>
-        </form>
+        <InputTodo todos={todos} setTodos={setTodos} getTodos={getTodos} />
+        <ListTodos todos={todos} />
       </main>
     </>
   )
