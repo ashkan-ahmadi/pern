f3671a06ecac98bb89b539a029e48c245cbb0522
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2023-09-15 13:43:46 +0200

    Update create todos migration file

--- server/db/migrations/20230914224225_create_todos_table.js
+++ server/db/migrations/20230914224225_create_todos_table.js
@@ -2,7 +2,7 @@ exports.up = async function (knex) {
   await knex.schema.createTable('todos', function (table) {
     table.increments()
     table.string('description').notNullable() // https://knexjs.org/guide/schema-builder.html#notnullable
-    table.timestamps(true, true) // https://knexjs.org/guide/schema-builder.html#timestamps
+    table.timestamps() // https://knexjs.org/guide/schema-builder.html#timestamps
   })
 }
 
